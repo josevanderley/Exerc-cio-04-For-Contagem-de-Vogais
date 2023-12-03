e1a11757bb41d796d0ea3952074876ee22026398
josevanderley <89553942+josevanderley@users.noreply.github.com>
2023-12-03 15:47:36 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_04_For_Contagem_de_Vogais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma palavra ou frase: ");
+            string entrada = Console.ReadLine().ToUpper();
+
+            int contadorVogais = 0;
+            for (int i = 0; i < entrada.Length; i++)
+            {
+                char caractere = entrada[i];
+                if (caractere == 'A' || caractere == 'E' || caractere == 'I' || caractere == 'O' || caractere == 'U')
+                {
+                    contadorVogais++;
+                }
+            }
+
+            Console.WriteLine($"Número de vogais na entrada: {contadorVogais}");
+
+            Console.ReadKey();
+        }
+    }
+}
+    
